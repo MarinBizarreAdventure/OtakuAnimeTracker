d181b435b764d52caa322bb7dedb4016743741f7
negai <negaimarin@gmail.com>
2024-06-21 01:09:07 +0300

    models

---
+++ OtakuTracker/OtakuTracker.Infrastructure/Models/RatingComplete.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OtakuTracker.Infrastructure.Models;
+
+public partial class RatingComplete
+{
+    public int UserId { get; set; }
+
+    public int AnimeId { get; set; }
+
+    public int? Rating { get; set; }
+
+    public virtual Anime Anime { get; set; } = null!;
+}
